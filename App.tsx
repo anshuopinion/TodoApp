18753ab32020e5a0c1a244667b655e478228d5be
Anshu Raj <anshusopinion@gmail.com>
2022-02-20 19:21:53 +0530

    todo input done

--- App.tsx
+++ App.tsx
@@ -1,115 +1,110 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  NativeBaseProvider,
+  Flex,
+  Checkbox,
   Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  FormControl,
+  Input,
+  ScrollView,
+  Button,
+  Stack,
+  VStack,
+} from 'native-base';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Formik} from 'formik';
+export default function App() {
+  const [todoLists, setTodoLists] = useState([
+    {
+      id: 1,
+      title: 'Todo 1',
+      completed: false,
+    },
+  ]);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const addTodo = (title: string) => {
+    const newTodo = {
+      id: todoLists.length + 1,
+      title,
+      completed: false,
+    };
+    setTodoLists([...todoLists, newTodo]);
   };
+  console.log(todoLists);
+
+  // const toggleTodo = (id: number) => {
+  //   const newTodoLists = todoLists.map(todo => {
+  //     if (todo.id === id) {
+  //       return {...todo, completed: !todo.completed};
+  //     }
+  //     return todo;
+  //   });
+  //   setTodoLists(newTodoLists);
+  // };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <SafeAreaView>
+        <Flex width="full">
+          <Text fontSize="xl" alignSelf="center" my={4}>
+            Todo App
+          </Text>
+
+          <ScrollView>
+            {todoLists.map(todo => (
+              <Flex
+                mb="2"
+                p={4}
+                shadow="2"
+                bg="white"
+                mx="4"
+                borderRadius="sm"
+                key={todo.id}>
+                <Checkbox value="true" colorScheme="green">
+                  {todo.title}
+                </Checkbox>
+              </Flex>
+            ))}
+          </ScrollView>
+          {/* <FormControl>
+            <Text>Add Todo</Text>
+            <Input
+              placeholder="Enter Todo"
+              onSubmitEditing={({nativeEvent}) =>
+                addTodo(nativeEvent.text.trim())
+              }
+            />
+          </FormControl> */}
+          <Formik initialValues={{todo: ''}} onSubmit={() => {}}>
+            {({handleBlur, handleChange, values, errors, handleSubmit}) => (
+              <VStack p="4" space="4">
+                <FormControl isInvalid={'todo' in errors}>
+                  <FormControl.Label>Task</FormControl.Label>
+                  <Input
+                    p="4"
+                    fontSize="md"
+                    onBlur={handleBlur('todo')}
+                    placeholder="Enter Todo"
+                    onChangeText={handleChange('todo')}
+                    value={values.todo}
+                  />
+                  <FormControl.ErrorMessage>
+                    {errors.todo}
+                  </FormControl.ErrorMessage>
+                </FormControl>
+                <Stack
+                  alignItems="flex-end"
+                  bg="red"
+                  position="relative"
+                  bottom="60"
+                  right="2">
+                  <Button onPress={handleSubmit}>Add Todo</Button>
+                </Stack>
+              </VStack>
+            )}
+          </Formik>
+        </Flex>
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+}
